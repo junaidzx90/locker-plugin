04d9e1c206f5f1ed55fc132b84857ef0f6e5e521
junaidzx90 <jsjunu@gmail.com>
2021-10-06 10:21:00 +0600

    update

--- admin/class-locker-plugin-admin.php
+++ admin/class-locker-plugin-admin.php
@@ -78,7 +78,7 @@ class Locker_Plugin_Admin {
 		global $post;
 		$lockable_types = get_option('lockable-post-types');
 		if(is_array($lockable_types)){
-			if(in_array(get_post_type($post), $lockable_types)){
+			if(in_array(get_post_type($post), $lockable_types) || (isset($_GET['page']) && $_GET['page'] == 'locker-plugin')){
 				wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/locker-plugin-admin.js', array( 'jquery' ), $this->version, false );
 			}
 		}
@@ -98,6 +98,9 @@ class Locker_Plugin_Admin {
 
 		add_settings_field( 'lockable-post-types', 'Where will show locker functionalities', [$this, 'lockable_post_types'], 'locker-plugin-page', 'locker-plugin-sections', '' );
 		register_setting( 'locker-plugin-sections', 'lockable-post-types');
+
+		add_settings_field( 'locker_for_all_user', 'For all type users', [$this, 'locker_for_all_user_cb'], 'locker-plugin-page', 'locker-plugin-sections', '' );
+		register_setting( 'locker-plugin-sections', 'locker_for_all_user');
 
 	}
 	// Menupage callback
@@ -136,6 +139,17 @@ class Locker_Plugin_Admin {
 		}
 	}
 
+	function locker_for_all_user_cb(){
+		$checked = 0;
+		if(get_option('locker_for_all_user')){
+			$checked = 1;
+		}else{
+			$checked = 0;
+		}
+		echo '<input type="checkbox" '.($checked?'checked':'').' id="locker_for_all_user" name="locker_for_all_user" value="'.$checked.'">';
+		echo '<small><p>Default for visitors</p></small>';
+	}
+
 	// Meta boxes
 	function locker_plugin_meta_boxes(){
 		$lockable_types = get_option('lockable-post-types');
@@ -147,47 +161,17 @@ class Locker_Plugin_Admin {
 	}
 
 	function locked_post_setting($post){
+		$today = date('Y-m-d h:i:sa');
+		$locked = get_post_meta($post->ID, 'is_post_locked', true);
+		$end = date('Y-m-d h:i:sa', strtotime($locked['end_date']));
+
 		echo '<div id="locker_setting">';
 
-		if(is_locked($post->ID)){
+		if(is_locked($post->ID) && strtotime($today) <= strtotime($end)){
 			echo '<span class="is_loked">
 			<button class="edit_locked">Edit</button>
 			</span>';
 		}
-
-		// Roles
-		echo '<div class="roles_settings">';
-
-		echo '<label for="user_roles">Select roles</label>';
-		echo '<select class="widefat" multiple name="user_roles[]" id="user_roles">';
-		$r = '';
- 
-		$editable_roles = array_reverse( get_editable_roles() );
-	
-		foreach ( $editable_roles as $role => $details ) {
-			$name = translate_user_role( $details['name'] );
-			// Preselect specified role.
-			$is_locked = get_post_meta($post->ID, 'is_post_locked', true);
-			if($is_locked && is_array($is_locked)){
-				$locked_roles = $is_locked['roles'];
-				$selected = '';
-				if(in_array($role, $locked_roles)){
-					$selected = $role;
-				}else{
-					$selected = '';
-				}
-			}
-
-			if ( $selected === $role ) {
-				$r .= "\n\t<option selected='selected' value='" . esc_attr( $role ) . "'>$name</option>";
-			} else {
-				$r .= "\n\t<option value='" . esc_attr( $role ) . "'>$name</option>";
-			}
-		}
-	
-		echo $r;
-		echo '</select>';
-		echo '</div>';// Roles
 
 		// Select dates
 		echo '<div class="select-dates">';
@@ -195,21 +179,21 @@ class Locker_Plugin_Admin {
 		$start_date = '';
 		$is_locked = get_post_meta($post->ID, 'is_post_locked', true);
 		if($is_locked && is_array($is_locked) && in_array($is_locked['start_date'], $is_locked) && !empty($is_locked['start_date'])){
-			$start_date = date('Y-m-d', strtotime($is_locked['start_date']));
+			$start_date = date('Y-m-d\TH:i:s', strtotime($is_locked['start_date']));
 		}
 
 		$end_date = '';
 		$is_locked = get_post_meta($post->ID, 'is_post_locked', true);
 		if($is_locked && is_array($is_locked) && in_array($is_locked['end_date'], $is_locked) && !empty($is_locked['end_date'])){
-			$end_date = date('Y-m-d', strtotime($is_locked['end_date']));
+			$end_date = date('Y-m-d\TH:i:s', strtotime($is_locked['end_date']));
 		}
 
 		echo '<label for="start-date">Start date (Optional)
-		<input class="widefat" value="'.$start_date.'" type="date" name="locker_lock-start-date" id="start-date">
+		<input class="widefat" value="'.$start_date.'" type="datetime-local" name="locker_lock-start-date" id="start-date">
 		</label>';
 
 		echo '<label for="end-date">End Date (Required)
-		<input class="widefat" value="'.$end_date.'" type="date" name="locker_lock-end-date" id="end-date">
+		<input class="widefat" value="'.$end_date.'" type="datetime-local" name="locker_lock-end-date" id="end-date">
 		</label>';
 
 		echo '</div>';// Select dates
@@ -217,7 +201,7 @@ class Locker_Plugin_Admin {
 		// Options
 		echo '<div class="options_settings">';
 
-		echo '<h4 class="lock-title">Options</h4>';
+		echo '<h4 class="lock-title">Option (Required)</h4>';
 		$selected = '';
 		$is_locked = get_post_meta($post->ID, 'is_post_locked', true);
 		if($is_locked && is_array($is_locked)){
@@ -234,22 +218,22 @@ class Locker_Plugin_Admin {
 			<span>Locked Contents</span>
 		</label>';
 
-		echo '<p style="color: #e76c6c">Note: Highly recommend disable comments if you using "Locked Contents"</p>';
+		if(strtotime($today) > strtotime($end)){
+			echo '<p style="color: #e76c6c">Date has expired.</p>';
+		}
+
 		echo '</div>';// Options
-		
 		echo '</div>';// Wrapper end
 	}
 
 	function locker_plugin_save_post($post_id){
 		$locked = array();
 
-		if(isset($_POST['user_roles']) && isset($_POST['locked_option'])){
-			$roles = $_POST['user_roles'];
+		if(isset($_POST['locked_option'])){
 			$option = $_POST['locked_option'];
 
-			if(count($roles) > 0 && !empty($option)){
+			if(!empty($option)){
 				$locked = [
-					'roles' => $roles,
 					'option' => $option
 				];
 				
@@ -276,55 +260,60 @@ class Locker_Plugin_Admin {
 
 	// Contents
 	function locker_plugin_the_content($the_content){
-		global $post, $current_user;
+		$lock = false;
+		if(get_option('locker_for_all_user')){
+			$lock = true;
+		}else{
+			if(!is_user_logged_in(  )){
+				$lock = true;
+			}
+		}
+		
+		if($lock){
+			global $post;
 
-		if(is_locked($post->ID)){
-			$is_locked = get_post_meta($post->ID, 'is_post_locked', true);
-			if(is_array($is_locked)){
-				$roles = $is_locked['roles']; //locked roles
+			if(is_locked($post->ID)){
+				$is_locked = get_post_meta($post->ID, 'is_post_locked', true);
+				if(is_array($is_locked)){
+					$today = date('Y-m-d h:i:sa');
 
-				foreach($current_user->roles as $role){
-					if(in_array($role, $roles)){
-						$today = date('Y-m-d');
+					$start_date = '';
+					if($is_locked['start_date']){
+						$start_date = date('Y-m-d h:i:sa', strtotime($is_locked['start_date']));
+					}
+					$end_date = '';
+					if($is_locked['end_date']){
+						$end_date = date('Y-m-d h:i:sa', strtotime($is_locked['end_date']));
+					}
 
-						$start_date = '';
-						if($is_locked['start_date']){
-							$start_date = date('Y-m-d', strtotime($is_locked['start_date']));
+					$is_start = true;
+					if($start_date){
+						if(strtotime($today) < strtotime($start_date)){
+							$is_start = false;
 						}
-						$end_date = '';
-						if($is_locked['end_date']){
-							$end_date = date('Y-m-d', strtotime($is_locked['end_date']));
-						}
-
-						$is_start = true;
-						if($start_date){
-							if(strtotime($today) < strtotime($start_date)){
-								$is_start = false;
-							}
-						}
-						
-						if(strtotime($today) <= strtotime($end_date) && $is_start){
-							$lock_option = $is_locked['option']; //lock option
-							if($lock_option === 'redirect'){
-								$redirect_url = esc_url( get_option('redirect_url') );
-								wp_safe_redirect( $redirect_url );
-								exit;
-							}else if($lock_option === 'contents'){
-								$lock_contents = get_option('locks-contents');
-								return $lock_contents;
-							}else{
-								return $the_content; //Default
-							}
+					}
+					
+					if(strtotime($today) <= strtotime($end_date) && $is_start){
+						$lock_option = $is_locked['option']; //lock option
+						if($lock_option === 'redirect'){
+							$redirect_url = esc_url( get_option('redirect_url') );
+							wp_redirect( $redirect_url );
+						}else if($lock_option === 'contents'){
+							$lock_contents = get_option('locks-contents');
+							return $lock_contents;
 						}else{
 							return $the_content; //Default
 						}
+					}else{
+						return $the_content; //Default
 					}
 				}
+			}else{
+				return $the_content; //Default
 			}
 		}else{
 			return $the_content; //Default
 		}
-		
 	}
 
 }
--- admin/js/locker-plugin-admin.js
+++ admin/js/locker-plugin-admin.js
@@ -6,4 +6,11 @@ jQuery(function( $ ) {
 		$(this).parent().remove()
 	})
 
+	$('#locker_for_all_user').on('change', function () {
+		if ($(this).val() == 0) {
+			$(this).val(1)
+		} else {
+			$(this).val(0)
+		}
+	})
 });
